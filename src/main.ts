9d026b9c2414c14b42e44869dacf6f6009277992
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-04-08 18:38:55 +0530

    Part -02

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,31 @@
 import './style.css'
 import typescriptLogo from './assets/typescript.svg';
 import viteLogo from './assets/vite.svg';
+import vitestLogo from './assets/vitest.svg';
 import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+const typescript: string = `https://cdn.jsdelivr.net/gh/offensive-vk/Icons@master/typescript/typescript-plain.svg`;
+const vitest: string = `https://cdn.jsdelivr.net/gh/offensive-vk/Icons@master/vitest/vitest-original.svg`;
+const vitejs: string = `https://cdn.jsdelivr.net/gh/offensive-vk/Icons@master/vitejs/vitejs-original.svg`;
+
+document.querySelector<HTMLDivElement>('#root')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="${viteLogo || vitejs}" class="logo" alt="Vitejs" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      <img src="${typescriptLogo || typescript}" class="logo vanilla" alt="TypeScript" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <a href="https://www.vitest.dev/" target="_blank">
+      <img src="${vitestLogo || vitest}" class="logo" alt="vitest" />
+    </a>
+    <h1>Vite + TypeScript ❤️ + Vitest</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+    <p class="read-the-docs">Built with ❤️ by 
+      <a href="https://github.com/offensive-vk/" target="_blank">Vedansh ✨</a> 
     </p>
   </div>
 `
-
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
